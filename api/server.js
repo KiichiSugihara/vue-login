a657f9c8b11d7a064edefeba2996369b37d509be
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2020-02-22 12:42:35 +0900

    add (api): add server.js mongodb

--- api/server.js
+++ api/server.js
@@ -17,12 +17,65 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const db = require('./app/models');
+const Role = db.role;
+
+db.mongoose
+  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('Successfully connect to MongoDB.');
+    initial();
+  })
+  .catch(err => {
+    console.error('Connection error', err);
+    process.exit();
+  });
+
 // simple route
 app.get('/', (req, res) => {
   res.json({
     message: 'Welcome to Node.js + MongoDB: JWT Authentication & Authorization'
   });
 });
+
+function initial() {
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new Role({
+        name: 'user'
+      }).save(err => {
+        if (err) {
+          console.log('error', err);
+        }
+
+        console.log("added 'user' to roles collection");
+      });
+
+      new Role({
+        name: 'moderator'
+      }).save(err => {
+        if (err) {
+          console.log('error', err);
+        }
+
+        console.log("added 'moderator' to roles collection");
+      });
+
+      new Role({
+        name: 'admin'
+      }).save(err => {
+        if (err) {
+          console.log('error', err);
+        }
+
+        console.log("added 'admin' to roles collection");
+      });
+    }
+  });
+}
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
